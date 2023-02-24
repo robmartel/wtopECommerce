020040d93329a537d0555b3e21228eee6b105b8a
robmartel <99683835+robmartel@users.noreply.github.com>
2023-02-24 15:38:09 -0500

    added product model schema

--- backend/models/ProductModel.js
+++ backend/models/ProductModel.js
@@ -1,11 +1,50 @@
-
-const mongoose = require("mongoose")
-const productSchema = mongoose.Schema({
+const mongoose = require('mongoose');
+const productSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-    }
-})
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+    },
+    reviewsNumber: {
+      type: Number,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+    attrs: [
+      { key: { type: String }, value: { type: String } },
+      // [{}, {}]
+    ],
+    images: [],
+    reviews: [],
+  },
+  {
+    timestamps: true,
+  }
+);
+productSchema.index();
 
-const Product = mongoose.model("Product", productSchema)
+const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product
+module.exports = Product;
