904cf27588a59128aa0a75ee7477cc803a5a8a5d
robmartel <99683835+robmartel@users.noreply.github.com>
2023-02-28 11:22:45 -0500

    added pagination file in config folder so could limit the amount of products displated on a particular page, added necessary line in product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,13 @@
+const Product = require('../models/ProductModel');
+const recordsPerPage = require('../config/pagination');
 
-const getProducts = (req, res) => {
-  res.send('Handling product routes, e.g. search for products');
+const getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find({}).sort({ name: 1 }).limit(recordsPerPage);
+    res.json({ products });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getProducts;
