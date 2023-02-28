c4f77ff4b3d283bdc207f24d872f51875734f736
robmartel <99683835+robmartel@users.noreply.github.com>
2023-02-28 12:00:24 -0500

    added filter api in product controller for price and rating

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,8 +3,27 @@ const recordsPerPage = require('../config/pagination');
 
 const getProducts = async (req, res, next) => {
   try {
+    let query = {};
+    let queryCondition = false;
+    let priceQueryCondition = {};
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(',') } };
+    }
+
+    if (queryCondition) {
+    query = {
+      $and: [priceQueryCondition, ratingQueryCondition],
+    };
+  }
+
+    //pagination
     const pageNum = Number(req.query.pageNum) || 1;
-    const totalProducts = await Product.countDocuments({});
 
     //sort by name, price, etc.
     let sort = {};
@@ -14,7 +33,9 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) }; //first set of [] around sortOpt is because you are using js dynamically for a key
       console.log(sort);
     }
-    const products = await Product.find({})
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
