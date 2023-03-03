bde0f2c4439a6bc7dde92febf53c8d2990b9b6f8
robmartel <99683835+robmartel@users.noreply.github.com>
2023-03-03 09:55:52 -0500

    created router and endpoint for getting products for admin and selecting only certain 'categories'

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {getProducts, getProductById, getBestsellers} = require('../controllers/productController');
+const {
+  getProducts,
+  getProductById,
+  getBestsellers,
+  adminGetProducts,
+} = require('../controllers/productController');
 
 router.get('/category/:categoryName/search/:searchQuery', getProducts); //anything after : is a dynamic value
 router.get('/category/:categoryName', getProducts);
 router.get('/search/:searchQuery', getProducts);
 router.get('/', getProducts);
 router.get('/bestsellers', getBestsellers);
-router.get('/:id', getProductById);
+router.get('/get-one:id', getProductById);
+
+//admin routes
+router.get('/admin', adminGetProducts);
 
 module.exports = router;
